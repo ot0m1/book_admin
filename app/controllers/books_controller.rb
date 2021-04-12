8bb6f7be19b3d8d4cda5b7402b89dfa09d0e2b48
ot0m1 <coding_addiction@yahoo.co.jp>
2021-04-12 22:06:17 +0900

    controllerの途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def show
     respond_to do |format|
       format.html
-      format.json
+      format.json { render json: @book }
     end
   end
 
